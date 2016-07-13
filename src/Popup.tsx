204ce665d1f3c870044e6e63fb64dd51b5ed7637
yiminghe <yiminghe@gmail.com>
2016-07-13 18:04:55 +0800

    pass triggerType

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import Cascader from './Cascader';
-import { getDefaultValue, COLS, noop, pick } from './utils';
+import {getDefaultValue, COLS, noop, pick} from './utils';
 import PopupPicker from 'rmc-picker/lib/Popup';
 import {PopupPickerPropsWeb} from 'rmc-picker/lib/PopupPickerTypes';
-import { CascaderProps, CascaderValue } from './CascaderTypes';
+import {CascaderProps, CascaderValue} from './CascaderTypes';
 
-const PROPS = ['onDismiss', 'children', 'style', 'WrapComponent',
-  'okText', 'dismissText', 'title', 'className', 'styles'];
+const PROPS = [
+  'onDismiss', 'children', 'style', 'WrapComponent',
+  'okText', 'dismissText', 'title', 'className',
+  'styles', 'triggerType',
+];
 
 export interface PopupCascaderProps extends PopupPickerPropsWeb, CascaderProps {
   popupPrefixCls?:string;
   pickerPrefixCls?:string;
   styles?:any;
-  visible?: boolean;
+  visible?:boolean;
   mode?:string;
   onPickerChange?:(date:CascaderValue) => void;
   onChange?:(date:CascaderValue) => void;
@@ -20,30 +23,33 @@ export interface PopupCascaderProps extends PopupPickerPropsWeb, CascaderProps {
 }
 
 export interface PopupCascaderState {
-  pickerValue?: CascaderValue;
-  visible?: boolean;
+  pickerValue?:CascaderValue;
+  visible?:boolean;
 }
 
 export default class PopupCascader extends React.Component<PopupCascaderProps, PopupCascaderState> {
   static defaultProps = {
-      popupPrefixCls: 'rmc-picker-popup',
-      onVisibleChange: noop,
-      cols: COLS,
-      onChange: noop,
-      onPickerChange: noop,
+    popupPrefixCls: 'rmc-picker-popup',
+    onVisibleChange: noop,
+    cols: COLS,
+    onChange: noop,
+    onPickerChange: noop,
   };
-  constructor(props: PopupCascaderProps) {
+
+  constructor(props:PopupCascaderProps) {
     super(props);
     this.state = {
       pickerValue: null,
       visible: this.props.visible || false,
     };
   }
+
   componentWillReceiveProps(nextProps) {
     if ('visible' in nextProps) {
       this.setVisibleState(nextProps.visible);
     }
   }
+
   onPickerChange = (value) => {
     console.log('inner onPickerChange', value);
     this.setState({
@@ -52,11 +58,12 @@ export default class PopupCascader extends React.Component<PopupCascaderProps, P
     this.props.onPickerChange(value);
   };
   onOk = () => {
-    const { value, cols, data } = this.props;
+    const {value, cols, data} = this.props;
     console.log('inner onOk', this.state.pickerValue);
     this.props.onChange(getDefaultValue(data,
       this.state.pickerValue || value, cols).filter(c => !!c));
   };
+
   setVisibleState(visible) {
     this.setState({
       visible,
@@ -67,11 +74,14 @@ export default class PopupCascader extends React.Component<PopupCascaderProps, P
       });
     }
   }
+
   getModal() {
-    const { data, cols, prefixCls,
+    const {
+      data, cols, prefixCls,
       pickerPrefixCls,
-      value } = this.props;
-    const extraProps: PopupCascaderProps = {
+      value
+    } = this.props;
+    const extraProps:PopupCascaderProps = {
       data,
     };
     if (pickerPrefixCls) {
@@ -87,6 +97,7 @@ export default class PopupCascader extends React.Component<PopupCascaderProps, P
       {...extraProps}
     />);
   }
+
   fireVisibleChange = (visible) => {
     if (this.state.visible !== visible) {
       if (!('visible' in this.props)) {
@@ -95,8 +106,9 @@ export default class PopupCascader extends React.Component<PopupCascaderProps, P
       this.props.onVisibleChange(visible);
     }
   };
+
   render() {
-    const props: any = pick(this.props, PROPS);
+    const props:any = pick(this.props, PROPS);
     props.prefixCls = this.props.popupPrefixCls;
     return (<PopupPicker
       {...props}
